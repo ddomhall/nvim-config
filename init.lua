2220633076e4a0c48d4a2e6c920f9661c22efcd4
ddomhall <domjshall@gmail.com>
2024-02-09 07:24:25 +0000

    new config

---
+++ init.lua
@@ -0,0 +1,15 @@
+require "opts"
+require "launch"
+
+spec("plugin.colorscheme")
+spec("plugin.treesitter")
+spec("plugin.telescope")
+spec("plugin.fugitive")
+spec("plugin.harpoon")
+spec("plugin.undotree")
+spec("plugin.lsp-zero")
+spec("plugin.mason")
+spec("plugin.gen")
+
+require "plugin.lazy"
+require "keymaps"
