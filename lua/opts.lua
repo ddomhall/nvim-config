3f63c16a38be72e95ef7a9278ae4eebc89f01c6c
ddomhall <domjshall@gmail.com>
2024-04-11 05:23:33 +0100

    update + sync clipboard

--- lua/opts.lua
+++ lua/opts.lua
@@ -16,11 +16,9 @@ vim.opt.incsearch = true
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undofile = true
--- Comment this line
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
--- And uncomment this one in case you use Windows
--- vim.opt.undodir = os.getenv("UserProfile") .. /".vim/undodir"
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 50
+vim.opt.clipboard = 'unnamedplus'
